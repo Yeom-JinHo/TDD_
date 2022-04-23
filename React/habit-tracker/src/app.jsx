de9d68972d8664c92da263b9ee418a0e37d86d56
Yeom-JinHo <pwlsghq@naver.com>
2022-04-23 23:20:26 +0900

    refactor:  React TDD 소스 로직

---
+++ React/habit-tracker/src/app.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useCallback } from "react";
+import { useState } from "react";
+import "./app.css";
+import Habits from "./components/habits";
+import Navbar from "./components/navbar";
+
+const App = ({ presenter }) => {
+  const [habits, setHabits] = useState(presenter.getHabits());
+
+  const handleIncrement = useCallback((habit) => {
+    // setHabits((habits) =>
+    //   habits.map((item) => {
+    //     if (item.id === habit.id) {
+    //       return { ...habit, count: habit.count + 1 };
+    //     }
+    //     return item;
+    //   })
+    // );
+    presenter.increment(habit, setHabits);
+  }, []);
+
+  const handleDecrement = useCallback((habit) => {
+    // setHabits((habits) =>
+    //   habits.map((item) => {
+    //     if (item.id === habit.id) {
+    //       const count = habit.count - 1;
+    //       return { ...habit, count: count < 0 ? 0 : count };
+    //     }
+    //     return item;
+    //   })
+    // );
+    presenter.decrement(habit, setHabits);
+  }, []);
+
+  const handleDelete = useCallback((habit) => {
+    // setHabits((habits) => habits.filter((item) => item.id !== habit.id));
+    presenter.delete(habit, setHabits);
+  }, []);
+
+  const handleAdd = useCallback((name) => {
+    // setHabits((habits) => [...habits, { id: Date.now(), name, count: 0 }]);
+    presenter.add(name, setHabits);
+  }, []);
+
+  const handleReset = useCallback(() => {
+    setHabits((habits) =>
+      habits.map((habit) => {
+        if (habit.count !== 0) {
+          return { ...habit, count: 0 };
+        }
+        return habit;
+      })
+    );
+  }, []);
+
+  return (
+    <>
+      <Navbar totalCount={habits.filter((item) => item.count > 0).length} />
+      <Habits
+        habits={habits}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        onDelete={handleDelete}
+        onAdd={handleAdd}
+        onReset={handleReset}
+      />
+    </>
+  );
+};
+
+export default App;
